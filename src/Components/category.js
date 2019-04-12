c1d593e8e5422417123447fe7e2eebe1e5ebadf0
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-12 03:43:23 -0400

    a lot changed

--- src/Components/category.js
+++ src/Components/category.js
@@ -1,44 +1,64 @@
+import EdiText from 'react-editext'
+import InlineEdit from 'react-ions/lib/InlineEdit'
 import React from 'react'
-import {Card} from 'react-bootstrap'
+import {useState} from 'react'
+import {Card,Collapse} from 'react-bootstrap'
 import Thumbnail from './thumbnail'
-export default function({thumbnails,editable,name})
+import Button from 'react-bootstrap/Button'
+export default function({thumbnails,editable,title})
 {
+	const [notCollapsed,setNotCollapsed]=useState(true)
+	const categoryFontSize           ='30px'
 	console.assert(thumbnails  !==undefined && Object.getPrototypeOf(thumbnails  )===Array .prototype)
 	// console.assert(numberOfRows!==undefined && Object.getPrototypeOf(numberOfRows)===Number.prototype)
 	return <Card>
-		<Card.Header style={{'text-align':'center'}}>{name}</Card.Header>
-		<Card.Body style={{'overflow':'scroll'}}>
-			<table>
-				<tr>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-					<td>
-						<Thumbnail/>
-					</td>
-				</tr>
-			</table>
-		</Card.Body>
+		<Card.Header>
+			<div style={{'display':'flex','flex-direction':'vertical'}}>
+				<Button variant="danger" onClick={()=>setNotCollapsed(!notCollapsed)}>{<i style={{'flex-grow': 0}} className="fas fa-minus-circle"/>}</Button>
+				<Button onClick={()=>setNotCollapsed(!notCollapsed)}>{notCollapsed?'▼':'▲'}</Button>
+				<div class='mx-auto' style={{'display':'inline-block'}}>{/*Center it*/}
+					<EdiText viewProps={{style:{'font-size':categoryFontSize}}}
+						// inputProps	={{style:{'font-size':categoryFontSize}}}
+							 value={title} type={'text'} onSave={alert}/>
+				</div>
+			</div>
+		</Card.Header>
+		<Collapse in={notCollapsed}>
+			<Card.Body style={{'overflow-x': 'scroll'}}>
+				<div>
+					<table>
+						<tr>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+							<td>
+								<Thumbnail/>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</Card.Body>
+		</Collapse>
 	</Card>;
 }

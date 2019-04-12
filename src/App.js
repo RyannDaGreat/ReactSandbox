e6ea4d861068542dc2020858a595e76598db1693
Elliot Tuck <eat3774@gmail.com>
2019-04-11 23:34:16 -0400

    Add menubar

--- src/App.js
+++ src/App.js
@@ -84,6 +84,7 @@
 
 import Thumbnail from './Components/thumbnail'
 import React, {Component} from 'react'
+import Menubar from './Components/menubar';
 
 class App extends Component
 {
@@ -97,6 +98,13 @@ class App extends Component
 					integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 					crossOrigin="anonymous"
 				/>
+				<link
+					rel="stylesheet"
+					href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+					integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+					crossorigin="anonymous"
+				/>
+				<Menubar loggedIn/>
 				<div className="App">
 					<Thumbnail/>
 				</div>

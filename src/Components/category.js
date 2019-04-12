922c8b0f5c466bbe3f04577d9517f5ddd8aec1f1
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-11 23:40:35 -0400

    a

--- src/Components/category.js
+++ src/Components/category.js
@@ -3,11 +3,10 @@ import {Card} from 'react-bootstrap'
 const defaultImageURL='https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg'
 const defaultTitle   ='Untitled'
 const defaultOnClick =()=>alert("defaultOnClick Test")
-export default function({generalBaseIds})
+export default function({thumbnails})
 {
 	console.assert(generalBaseIds!==undefined && Object.getPrototypeOf(generalBaseIds)===Array.prototype)
-	return <Card style={{width: '18rem',cursor:'pointer'}} onClick={onClick}>
-		<Card.Img variant="top" src={imageURL}/>
-		<Card.Footer style={{'text-align':'center'}}>{title}</Card.Footer>
-	</Card>
+	return <div style={''}>
+
+	</div>
 }

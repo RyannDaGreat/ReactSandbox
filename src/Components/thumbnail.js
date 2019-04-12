c253b3ad68f884a88d36ce80dbdfb4665e8118d9
Elliot Tuck <eat3774@gmail.com>
2019-04-12 01:22:15 -0400

    Hotfix

--- src/Components/thumbnail.js
+++ src/Components/thumbnail.js
@@ -5,15 +5,9 @@ const defaultTitle   ='Untitled'
 const defaultOnClick =()=>alert("defaultOnClick Test")
 export default function({title=defaultTitle, imageURL=defaultImageURL, onClick=defaultOnClick})
 {
-<<<<<<< HEAD
-	console.assert(title   !==undefined && Object.getPrototypeOf(title   )===String  .prototype)
-	console.assert(imageURL!==undefined && Object.getPrototypeOf(imageURL)===String  .prototype)
-	console.assert(onClick !==undefined && Object.getPrototypeOf(imageURL)===Function.prototype)
-=======
 	console.assert(title!==undefined && Object.getPrototypeOf(title)===String.prototype)
 	console.assert(imageURL!==undefined && Object.getPrototypeOf(imageURL)===String.prototype)
 	console.assert(onClick!==undefined && Object.getPrototypeOf(onClick)===Function.prototype)
->>>>>>> 365ae6e17aff940dc3906db0f9591142037401e6
 	return <Card style={{width: '18rem', cursor: 'pointer'}} onClick={onClick}>
 		<Card.Img variant="top" src={imageURL}/>
 		<Card.Footer style={{'textAlign': 'center'}}>{title}</Card.Footer>

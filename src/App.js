729d85a84a20345ab7e7290282dfbbb953a7fc64
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-11 23:42:03 -0400

    aoisjdasd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Thumbnail from './Components/thumbnail'
 import React, {Component} from 'react'
-import Menubar from './Components/menubar';
 
 class App extends Component
 {
 	render()
 	{
-<<<<<<< HEAD
 		return <>
 			<link
 				rel="stylesheet"
@@ -18,28 +16,6 @@ class App extends Component
 				<Thumbnail/>
 			</div>
 		</>
-=======
-		return (
-			<>
-				<link
-					rel="stylesheet"
-					href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-					integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-					integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-					crossorigin="anonymous"
-				/>
-				<Menubar loggedIn/>
-				<div className="App">
-					<Thumbnail/>
-				</div>
-			</>
-		)
->>>>>>> 42a5c473971eb6caff67be0b8c38181cacd1d799
 	}
 }
 

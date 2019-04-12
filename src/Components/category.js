51ed309579d25c211b098cf7f81cbce7647a63cb
RyanBurgert219 <sqrtryan@gmail.com>
2019-04-12 04:09:15 -0400

    commit

--- src/Components/category.js
+++ src/Components/category.js
@@ -2,63 +2,72 @@ import EdiText from 'react-editext'
 import InlineEdit from 'react-ions/lib/InlineEdit'
 import React from 'react'
 import {useState} from 'react'
-import {Card,Collapse} from 'react-bootstrap'
+import {Card, Collapse, Modal} from 'react-bootstrap'
 import Thumbnail from './thumbnail'
 import Button from 'react-bootstrap/Button'
-export default function({thumbnails,editable,title})
+export default function({thumbnails, editable, title})
 {
+	const categoryFontSize='30px'
 	const [notCollapsed,setNotCollapsed]=useState(true)
-	const categoryFontSize           ='30px'
-	console.assert(thumbnails  !==undefined && Object.getPrototypeOf(thumbnails  )===Array .prototype)
+	const [askIfDelete ,setAskIfDelete ]=useState(true)
+	console.assert(thumbnails!==undefined && Object.getPrototypeOf(thumbnails)===Array.prototype)
 	// console.assert(numberOfRows!==undefined && Object.getPrototypeOf(numberOfRows)===Number.prototype)
+	const deleteAsker=<div class='mx-auto' >
+			Are you sure you want to delete this category?
+			<span>
+			<Button variant={'danger'}>Yes</Button>
+			<Button onClick={()=>setAskIfDelete(false)}>No</Button>
+			</span>
+	</div>
+	const cards=<Card.Body style={{'overflow-x': 'scroll'}}>
+		<div>
+			<table>
+				<tr>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+					<td>
+						<Thumbnail/>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</Card.Body>
 	return <Card>
 		<Card.Header>
-			<div style={{'display':'flex','flex-direction':'vertical'}}>
-				<Button variant="danger" onClick={()=>setNotCollapsed(!notCollapsed)}>{<i style={{'flex-grow': 0}} className="fas fa-minus-circle"/>}</Button>
-				<Button onClick={()=>setNotCollapsed(!notCollapsed)}>{notCollapsed?'▼':'▲'}</Button>
-				<div class='mx-auto' style={{'display':'inline-block'}}>{/*Center it*/}
-					<EdiText viewProps={{style:{'font-size':categoryFontSize}}}
+			<div style={{'display': 'flex', 'flex-direction': 'vertical'}}>
+				<Button variant="danger" onClick={()=>setAskIfDelete(true)}>{<i style={{'flex-grow': 0}} className="fas fa-minus-circle"/>}</Button>
+				<Button onClick={()=>setNotCollapsed(!notCollapsed)}>{notCollapsed ? '▼' : '▲'}</Button>
+				<div class='mx-auto'>{/*Center it*/}
+					<EdiText viewProps={{style: {'font-size': categoryFontSize}}}
 						// inputProps	={{style:{'font-size':categoryFontSize}}}
 							 value={title} type={'text'} onSave={alert}/>
 				</div>
 			</div>
 		</Card.Header>
 		<Collapse in={notCollapsed}>
-			<Card.Body style={{'overflow-x': 'scroll'}}>
-				<div>
-					<table>
-						<tr>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-							<td>
-								<Thumbnail/>
-							</td>
-						</tr>
-					</table>
-				</div>
-			</Card.Body>
+			{askIfDelete ? deleteAsker : cards}
 		</Collapse>
-	</Card>;
+	</Card>
 }
